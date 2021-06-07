a0f5671da9e47733e78f7a2bfe3e8ca6430fc44f
howard.yu <howard.yu@wiadvance.com>
2021-06-07 15:31:27 +0800

    Add News API

---
+++ NewsStatistics/Controllers/NewsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using NewsStatistics.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewsStatistics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        static string newsApiKey = "1016900e42d14800a27902842a18b0c8";
+
+        // GET: api/<NewsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            NewsService newsService = new NewsService();
+
+            var json = newsService.getNews();
+
+            return Ok(json);
+        }
+
+        // GET api/<NewsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<NewsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NewsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NewsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NewsStatistics/Services/NewsService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+
+namespace NewsStatistics.Services
+{
+    public class NewsService
+    {
+        public string getNews()
+        {
+            string jsonData = string.Empty;
+            var newsApiClient = new NewsApiClient("1016900e42d14800a27902842a18b0c8");
+            var topNews = newsApiClient.GetTopHeadlines(new TopHeadlinesRequest {
+                Category = Categories.Business,
+                Country = Countries.TW,
+                Q = "半導體"
+            });
+
+            if (topNews.Status == Statuses.Ok)
+            {
+                Console.WriteLine(topNews.TotalResults);
+                jsonData = JsonConvert.SerializeObject(topNews.Articles);
+            }
+            return jsonData;
+
+            //var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            //{
+            //    Q = "Apple",
+            //    SortBy = SortBys.Popularity,
+            //    Language = Languages.EN,
+            //    From = new DateTime(2018, 1, 25)
+            //});
+            //if (articlesResponse.Status == Statuses.Ok)
+            //{
+            //    // total results found
+            //    Console.WriteLine(articlesResponse.TotalResults);
+            //    // here's the first 20
+            //    foreach (var article in articlesResponse.Articles)
+            //    {
+            //        // title
+            //        Console.WriteLine(article.Title);
+            //        // author
+            //        Console.WriteLine(article.Author);
+            //        // description
+            //        Console.WriteLine(article.Description);
+            //        // url
+            //        Console.WriteLine(article.Url);
+            //        // published at
+            //        Console.WriteLine(article.PublishedAt);
+            //    }
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
